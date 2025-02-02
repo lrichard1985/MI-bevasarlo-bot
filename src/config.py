500e585b610da1762b2d3b8e3218390ac4abb4c0
lrichard1985 <98228231+lrichard1985@users.noreply.github.com>
2025-02-02 14:37:23 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
 # Konfigurációs változók és beállítások
+# src/config.py
+from security import get_secret
+
+class Config:
+    VERIFY_TOKEN = get_secret("VERIFY_TOKEN")
+    PAGE_ACCESS_TOKEN = get_secret("PAGE_ACCESS_TOKEN")
+    GOOGLE_CLOUD_PROJECT = get_secret("GOOGLE_CLOUD_PROJECT")
+    SHEET_NAME = "Shopping_List"  # Ez marad az .env-ből, ha szükséges
