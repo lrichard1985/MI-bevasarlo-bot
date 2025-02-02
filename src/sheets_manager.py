76426752531cdb78a21ef3e72447f98f8d63c8eb
lrichard1985 <98228231+lrichard1985@users.noreply.github.com>
2025-02-02 15:25:54 +0100

    Update sheets_manager.py
    Ebben a módosított verzióban a get_secret függvényt használjuk a security.py modulból, hogy lekérjük a Google Cloud Service Account JSON kulcsot a titokból, amit a Secret Managerben tárolunk.

--- src/sheets_manager.py
+++ src/sheets_manager.py
@@ -1,13 +1,17 @@
-# Google Sheets kezelése
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import SHEET_NAME
+from security import get_secret
+import json
 
 def get_sheet():
     """Csatlakozik a Google Sheets-hez és visszaadja a bevásárlólista táblázatot."""
     scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
-    creds = ServiceAccountCredentials.from_json_keyfile_name('GOOGLE_CREDENTIALS_SECRET_NAME', scope)
+    
+    # A kulcsot közvetlenül a Secret Managerből töltjük be
+    creds_json = json.loads(get_secret('GOOGLE_CREDENTIALS_SECRET_NAME'))  # A GOOGLE_CREDENTIALS_SECRET_NAME helyére írd be a tényleges titok nevét
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
+    
     client = gspread.authorize(creds)
     sheet = client.open(SHEET_NAME).sheet1  # A táblázat első lapjára csatlakozik
 
